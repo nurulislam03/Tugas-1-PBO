1abb21f288a82e7a5771afce7b71780574c7bb52
nurulislam03 <79521393+nurulislam03@users.noreply.github.com>
2021-11-04 21:54:19 +0700

    Add files via upload

---
+++ BelajarJava2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class BelajarJava2 {
+    public static void main(String[] args) {
+        String nama = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        
+        JOptionPane.showMessageDialog(null,"Belajar "+nama+" Sangat mudah");
+    }
+}
